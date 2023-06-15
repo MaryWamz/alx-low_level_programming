6127d782543929906468ad1c9052efdbf426329e
MaryWamz <mariwamuyu@gmail.com>
2023-06-15 12:03:51 -0700

    put instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - printing a line in C without using printf ending with a zero value
+ *
+ * Return: Always 0 (True)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"");
+	return (0);
+}
+
